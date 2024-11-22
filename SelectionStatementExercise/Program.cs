38ea345cf88cbd98ce7cf14df7da0e713c551b7c
Amy Galbicsek <acgalbicsek@gmail.com>
2024-11-22 13:26:04 -0900

    Updated Work

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            Random r = new Random(); //This line creates a new `Random` object, which is used to generate random numbers.
+            int favNumber = r.Next(1, 11);  //This line generates a random integer between 1 and 999 and assigns it to the `favNumber` variable.
+            int userInput = 0;
+
+            while (userInput != favNumber)
+            {
+
+
+                Console.WriteLine("Guess my favorite number:");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {userInput}");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"Too High!\nYour guess: {userInput}");
+                }
+                else
+                {
+                    Console.WriteLine($" Yup! You got it!");
+                }
+
+            }
         }
     }
 }
